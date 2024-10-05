312cd56c5b853fdf2c4a85b4bf624b8bee759a32
Salem <salembenhalima92@gmail.com>
2024-10-05 08:36:16 +0100

    Minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import Home from './homepage.jsx';
-import Salem from './pages/a.jsx';
+import All from './pages/all.jsx';
+import ResetPassword from './pages/reset.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="" element={<Home />} />
-        <Route path="/a" element={<Salem />} />
-        <Route path="/owner" element={<Salem />} />
-        <Route path="/admin" element={<Salem />} />
-        <Route path="/scods" element={<Salem />} />
-        <Route path="/users" element={<Home />} />
+        <Route path="/all" element={<All />} />
+        <Route path="/reset" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
 );
